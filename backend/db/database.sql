73ada35610af6d9a3fbde9f86d3328cc27d193cd
ComfyClicks <79934542+ComfyClicks@users.noreply.github.com>
2024-04-02 18:32:46 -0400

    corrected db

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -1,18 +1,16 @@
 CREATE DATABASE job_journey_db;
-USE jobJourneyDB;
+USE job_journey_db;
 
 CREATE TABLE users (
   user_id INT AUTO_INCREMENT,
   username VARCHAR(255),
   first_name VARCHAR(255),
   last_name VARCHAR(255),
-  PRIMARY KEY (user_id),
-  FOREIGN KEY (job_id) REFERENCES job(job_id)
+  PRIMARY KEY (user_id)
 );
 
 CREATE TABLE job (
   job_id INT AUTO_INCREMENT,
-  company_id INT,
   company_name VARCHAR(255),
   job_title VARCHAR(255),
   job_location VARCHAR(255),
@@ -20,14 +18,13 @@ CREATE TABLE job (
   job_url VARCHAR(2083),
   job_type VARCHAR(255),
   salary INT,
-  PRIMARY KEY (job_id),
-  FOREIGN KEY (user_id) REFERENCES users(user_id)
+  PRIMARY KEY (job_id)
 );
 
 CREATE TABLE application (
   app_id INT AUTO_INCREMENT,
   user_id INT,
-  posting_id INT,
+  job_id INT,
   application_date DATE,
   application_method VARCHAR(255),
   application_status VARCHAR(255),
@@ -36,5 +33,6 @@ CREATE TABLE application (
   follow_up_actions TEXT,
   additional_notes TEXT,
   PRIMARY KEY (app_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
   FOREIGN KEY (job_id) REFERENCES job(job_id)
 );
