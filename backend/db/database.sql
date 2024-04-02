028c5a9bf12c0be456c9aa6e47ea165e1a4a2e49
ComfyClicks <79934542+ComfyClicks@users.noreply.github.com>
2024-04-02 15:25:10 -0400

    corrected database

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -7,9 +7,9 @@ CREATE TABLE users (
   first_name VARCHAR(255),
   last_name VARCHAR(255),
   PRIMARY KEY (user_id)
-)
+);
 
-CREATE TABLE job_application (
+CREATE TABLE application (
   app_id INT AUTO_INCREMENT,
   user_id INT,
   posting_id INT,
@@ -22,24 +22,24 @@ CREATE TABLE job_application (
   additional_notes TEXT,
   PRIMARY KEY (app_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (job_id) REFERENCES job_posting(job_id)
-)
+  FOREIGN KEY (job_id) REFERENCES job(job_id)
+);
 
-CREATE TABLE job_posting (
+CREATE TABLE job (
   job_id INT AUTO_INCREMENT,
   company_id INT,
   job_title VARCHAR(255),
   job_location VARCHAR(255),
   job_description TEXT,
-  job_url VARCHAR,
+  job_url VARCHAR(2083),
   job_type VARCHAR(255),
   salary INT,
-  PRIMARY KEY (posting_id),
-  FOREIGN KEY (company_id) REFERENCES company_id(company)
-)
+  PRIMARY KEY (app_id),
+  FOREIGN KEY (company_id) REFERENCES company(company)
+);
 
 CREATE TABLE company (
   company_id INT AUTO_INCREMENT,
-  name VARCHAR,
+  company_name VARCHAR(255),
   PRIMARY KEY (company_id)
-)
+);
